c266298614db72a5805bd6d46533f8934a6b1faf
Edward Nunez <edwardnnz@gmail.com>
2025-12-03 22:09:35 -0500

    refactor: update project structure, versioning, and add initial game setup with Phaser

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import path from "node:path";
+import process from "node:process";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    root: "src",
+    publicDir: "../public",
+    build: {
+        outDir: "../dist"
+    },
+    resolve: {
+        alias: { "/src": path.resolve(process.cwd(), "src") }
+    },
+})
